58e1bf2b746e6dcc906c3e6f4f46d1b01cdeab71
Jason rader <Jasonmrader@outlook.com>
2023-09-09 14:13:59 -0400

    refactoring code: added more lists to singleton class

--- Staff Performance Class Library/DataSingleton.cs
+++ Staff Performance Class Library/DataSingleton.cs
@@ -11,7 +11,10 @@ namespace Staff_Performance_Class_Library
         private static DataSingleton instance;
         public List<Employee> Employees { get; private set; }
         public List<Shift> Shifts { get; private set; }
-        public List<Incident> Incidents { get; private set; }
+        //public List<Incident> Incidents { get; private set; }
+        public List<Position> Positions { get; private set; }     
+        public List<EmployeeShift> EmployeeShifts { get ; private set; }
+        public List<Activity> Activities { get; private set; }
         public List<string> ignoredExcelCells { get; private set; }
         // ... other lists
 
@@ -19,8 +22,12 @@ namespace Staff_Performance_Class_Library
         {
             Employees = new List<Employee>();
             Shifts = new List<Shift>();
-            Incidents = new List<Incident>();
-            // ... initialize other lists
+            //Incidents = new List<Incident>();
+            Positions = new List<Position>();
+            EmployeeShifts = new List<EmployeeShift>();
+            Activities = new List<Activity>();
+            ignoredExcelCells = new List<string>();
+            
             LoadDataFromDatabase();
         }
 
@@ -37,7 +44,26 @@ namespace Staff_Performance_Class_Library
         private void LoadDataFromDatabase()
         {
             Employees = SqliteDataAccess.LoadEmployees();
+            Shifts = SqliteDataAccess.LoadShifts();
+            Positions = SqliteDataAccess.LoadPositions();
+            //Incidents
+            //EmployeeShifts = SqliteDataAccess.LoadEmployeeShifts();
+            Activities = SqliteDataAccess.LoadActivities();
             ignoredExcelCells = SqliteDataAccess.LoadExcelIgnore();
+
+            BuildEmployeeShifts();
+        }
+        private void BuildEmployeeShifts()
+        {
+            EmployeeShifts.Clear();
+            foreach (Employee employee in Employees)
+            {
+                foreach (EmployeeShift employeeShift in employee.EmployeeShifts)
+                {
+                    employeeShift.Employee = employee;
+                    EmployeeShifts.Add(employeeShift);
+                }
+            }
         }
 
         public void SaveDataToDatabase()
